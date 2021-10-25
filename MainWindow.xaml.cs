11411d1f2874e07e8272333492a795ef757ff53b
geloidA <alexkolpak2003@gmail.com>
2021-10-25 20:09:31 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace passwordchecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var passwordChecker = new PasswordChecker("[0-9]", "[a-z]", "[!@#$%^]", "[A-Z]");
+
+            if(passwordChecker.Check(passwordBox.Password))
+            {
+                File.WriteAllText(@"C:\Users\Public\password.txt", passwordBox.Password);
+                MessageBox.Show(@"Файл с паролем сохранён в C:\Users\Public");
+            }
+        }
+    }
+}
---
+++ PasswordChecker.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace passwordchecker
+{
+    class PasswordChecker
+    {
+        private string[] patterns;
+
+        public PasswordChecker(params string[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Check(string password)
+        {
+            if (password.Length < 6)
+                return ReturnFalseWithMessage("Минимум 6 символов");
+
+            foreach(var pattern in patterns)
+            {
+                if (!Regex.IsMatch(password, pattern))
+                    return ReturnFalseWithMessage($"Пароль не содержит {pattern}");
+            }
+            return true;
+        }
+
+        private bool ReturnFalseWithMessage(string message)
+        {
+            MessageBox.Show(message);
+            return false;
+        }
+    }
+}
